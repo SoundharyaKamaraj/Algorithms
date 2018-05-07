2b78b452c9ba5d41d7a5021cfd983121c97b3567
Soundharya <ksoundharya@gmail.com>
2018-05-07 12:28:04 -0400

    Completed logic for roman numerals

--- src/com/learn/algorithms/romannumerals/ClassicRomanNumerals.java
+++ src/com/learn/algorithms/romannumerals/ClassicRomanNumerals.java
@@ -10,15 +10,16 @@ import java.util.Scanner;
  *
  */
 public class ClassicRomanNumerals {
-	public static final String ROMAN_ONE = "I";
-	public static final String ROMAN_FIVE = "V";
-	public static final String ROMAN_TEN = "X";
-	public static final String ROMAN_FIFTY = "L";
-	public static final String ROMAN_HUNDRED = "C";
-	public static final String ROMAN_FIVEHUNDRED = "L";
-	public static final String ROMAN_THOUSAND = "M";
-	public static final String EMPTY_STRING = "";
-
+	
+	//String array containing 13 roman symbols
+    
+    private static String[] romanSymbols = {"M", "CM", "D", "CD", "C", "XC", "L", "XL", "X", "IX", "V", "IV", "I"};
+     
+    //int array containing 13 decimal numbers
+     
+   private static int[] decimals = {1000, 900, 500, 400, 100, 90, 50, 40, 10, 9, 5, 4, 1};
+	
+   private static String[] oldRomanSymbols = {"M", "DCCCC", "D", "CCCC", "C", "LXXXX", "L", "XXXX", "X", "VIIII", "V", "IIII", "I"};
 	/**
 	 * @param args
 	 */
@@ -43,92 +44,44 @@ public class ClassicRomanNumerals {
 		return convertToOldApproach(userOne);
 	}
 
-	private static String convertToOldApproach(int userOne) {
-		int lengthOfDigit = (int) Math.log10(userOne) + 1;
-		System.err.println(lengthOfDigit);
-		String firstRomanValue = EMPTY_STRING;
-		String remainngRomanValue = EMPTY_STRING;
+	private static String convertToOldApproach(int inputNumber) {
+		String roman="";
 		
-		if (lengthOfDigit == 1) {
-			if (userOne >= 1 && userOne < 5) {
-				for (int i = 0; i < userOne; i++) {
-					firstRomanValue = firstRomanValue + ROMAN_ONE;
-				}
-				return firstRomanValue;
-			} 
-			else{
-				firstRomanValue = firstRomanValue + ROMAN_FIVE;
-				remainngRomanValue=fetchRemainRomanValue(userOne-5,firstRomanValue);
-				return firstRomanValue+remainngRomanValue;
+		for(int j=0;j<13;j++){
+			while(inputNumber>=decimals[j]){
+				inputNumber = inputNumber - decimals[j];
+				roman =roman+oldRomanSymbols[j];
 			}
-
-		} 
-		else if (lengthOfDigit == 2) {
-			if (userOne >= 10 && userOne < 50) {
-				firstRomanValue = firstRomanValue + ROMAN_TEN;
-				remainngRomanValue=fetchRemainRomanValue(userOne-10,firstRomanValue);
-				return firstRomanValue+remainngRomanValue;
-				}
-				else if (userOne >= 50 && userOne < 100) {
-					firstRomanValue=firstRomanValue + ROMAN_FIFTY;
-					remainngRomanValue=fetchRemainRomanValue(userOne-50,firstRomanValue);
-					return firstRomanValue+remainngRomanValue;
-				}		
-		} else if (lengthOfDigit == 3) {
-
-		} else if (lengthOfDigit == 4) {
-
-		} else if (lengthOfDigit == 5) {
-
 		}
-
-		return null;
+		return roman;
 	}
 
-	private static String fetchRemainRomanValue(int remainingTotal, String firstRomanValue) {
-		int modulusValue = fetchModulusValue(remainingTotal, 10);
-		String remainNumerals=EMPTY_STRING;
-		if(modulusValue==0){
-			for(double i=0;i<=Math.log(remainingTotal);i++){
-				remainNumerals = remainNumerals + firstRomanValue;
-			}
+	private static String convertToModernSystem(int inputNumber) {
+		String roman="";
+		 //Copying inputNumber into copyOfInputNumber
+        
+		if(inputNumber<=0 &&  inputNumber>=3000){
+			System.err.println("Please enter an valid number");
 		}
-		
 		else{
-			int pendingNumerals=remainingTotal-modulusValue;
 			
-			remainNumerals= CalculateModulusNumerals(modulusValue);
-		}
-		return remainNumerals;
+			for (int i = 0; i < 13; i++) 
+            {
+                while(inputNumber >= decimals[i])
+                {
+                	System.out.println("inputNumber "+inputNumber);
+                    inputNumber = inputNumber - decimals[i];
+                    roman = roman + romanSymbols[i];
+                    System.out.println("Roman Equivalent Of "+inputNumber+" is : "+roman);
+                }
+            }
+             
+        }
 		
-	}
-
-	private static String CalculateModulusNumerals(int modulusValue) {
-		String secondRomanValue = EMPTY_STRING;
-		if (modulusValue == 5){
-			secondRomanValue = secondRomanValue + ROMAN_FIVE;
-		}
-		else if (modulusValue < 5) {
-			for (int i = 0; i < modulusValue; i++) {
-				secondRomanValue = secondRomanValue + ROMAN_ONE;
-			}
-		}
-		else{
-			secondRomanValue = secondRomanValue + ROMAN_FIVE;
-			for (int i = 0; i < modulusValue-5; i++) {
-				secondRomanValue = secondRomanValue + ROMAN_ONE;
-			}
-		}
-		return secondRomanValue;
-	}
-
-	private static String convertToModernSystem(int userOne) {
-
-		return null;
+		return roman;
 	}
 	
-	private static int fetchModulusValue(int actualValue, int modulusValue) {
-		return actualValue % modulusValue;
-	}
+	
+	
 
 }
