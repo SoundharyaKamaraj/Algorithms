66456fd247fd5f59cc2db9966de8d70cc0698f15
Soundharya <ksoundharya@gmail.com>
2018-05-09 22:41:56 -0400

    Added few more algorithms from woman who code boston

---
+++ src/com/learn/algorithms/opendoor/DoorStateAlgorithm.java
@@ -0,0 +1,48 @@
+/**
+ * 
+ */
+package com.learn.algorithms.opendoor;
+
+import java.util.LinkedList;
+import java.util.List;
+
+/**
+ * @author Soundharya
+ *
+ */
+public class DoorStateAlgorithm {
+	
+	private static boolean[] doorStatuses = new boolean[100];
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		int n = 100;
+		for (int i=0; i < doorStatuses.length; i++) {
+			List<Integer> factors = getFactors(i+1);
+			int count = 0;
+			for (int factor : factors) {
+				if (factor <= n) {
+					count++;
+				}
+			}
+			if (count%2 == 1) {
+				doorStatuses[i] = true;
+			}
+			System.out.println(String.format("door[%s] is %s", i+1, doorStatuses[i] ? "open" : "closed"));
+		}
+		
+	}
+	
+	private static List<Integer> getFactors(int x) {
+		List<Integer> list = new LinkedList<>();
+		for (int i=1; i<=x; i++) {
+			if (x%i == 0) {
+				list.add(i);
+			}
+		}
+		return list;
+	}
+
+}
